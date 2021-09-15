3e547e34dd0479f700617ae5799c7cffc1760265
Chris Ayers <clayers@gmail.com>
2021-09-14 23:51:27 -0500

    added hostname

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
+const os = require("os");
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const HOSTNAME = os.hostname();
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World - ' + `Running on ${HOSTNAME}`);
 });
 
 app.listen(PORT, HOST);
